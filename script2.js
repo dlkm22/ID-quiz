8a1d375be7c3f420fa55958c0f38d338b997e96b
dlkm22 <S10242633@connect.np.edu.sg>
2023-01-26 22:09:42 +0800

    implementation of login page and touchups

---
+++ script2.js
@@ -0,0 +1,90 @@
+$("#add-success-msg").hide();
+$("#add-unsuccessful-msg").hide();
+     $(document).ready(function () { 
+      $("#submit").on("click", function (e) { 
+        // prevent default action of the button
+          e.preventDefault();
+  
+          let email = $("#eMail").val();
+          let password = $("#passWord").val();
+  
+          let jsondata = { 
+              "email": email,
+              "password": password
+  
+          };
+          
+          var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://userinformation-531d.restdb.io/rest/userinfomatio",
+            "method": "GET",
+            "headers": {
+              "content-type": "application/json",
+              "Accept": "application/json",
+              "x-apikey": "63c8a032969f06502871afee",
+              "cache-control": "no-cache"
+            }
+          }
+          
+          $.ajax(settings).done(function (response) {
+            console.log(response);
+            //console.log(response[0].email);
+            //console.log(response[0].password);
+
+            for (let i = 0; i < response.length; i++) {
+                console.log(response[i].email);
+
+                if (email === response[i].email && password === response[i].password){
+                    $("#add-success-msg").show();
+                    function reDirect() {
+                        window.location.href="index.html";  
+                }
+                    reDirect();
+                }
+
+                else{
+                    $("#add-unsuccessful-msg").show();
+                    console.log("INCORRECT LOGIN DETAILS")
+                }
+              }
+          });
+     
+          });
+  
+      })
+
+      
+
+    
+
+
+
+
+      
+
+
+  
+
+
+
+
+
+
+/*
+  var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://userinformation-531d.restdb.io/rest/userinfomatio",
+    "method": "GET",
+    "headers": {
+      "content-type": "application/json",
+      "x-apikey": "<your CORS apikey here>",
+      "cache-control": "no-cache"
+    }
+  }
+  
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+  });
+  */
